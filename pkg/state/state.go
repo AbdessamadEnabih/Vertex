3d344e5dae2a408757a3df4359668c419c3cd322
Abdessamad enabih <a.enabih@gmail.com>
2024-09-01 17:17:41 +0100

    Add Set and State

--- pkg/state/state.go
+++ pkg/state/state.go
@@ -6,10 +6,18 @@ import (
 )
 
 type State struct {
-	data map[string]string
+	data map[string]interface{} // To allow any type
 	mu   sync.Mutex
 }
 
-func Insert() {
-	fmt.Print("state")
+func newState() *State  {
+	return &State{
+		data : make(map[string]interface{}, 1000)
+	}
+}
+
+func (s *State) Set(key string, value interface{}) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[key] = value
 }
