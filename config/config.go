3b422097a83bfec3fe7f872de17764b8640f5ffd
Abdessamad Enabih <a.enabih@gmail.com>
2024-10-13 15:43:05 +0100

    Optimize Structs for better memory usage

--- config/config.go
+++ config/config.go
@@ -1,58 +1,48 @@
 package config
 
 import (
+	"gopkg.in/yaml.v2"
 	"log"
 	"os"
 	"path/filepath"
-
-	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
+	Logging struct {
+		Level      string `yaml:"level"`
+		File       string `yaml:"file"`
+		MaxSize    string `yaml:"max_size"`
+		MaxAge     string `yaml:"max_age"`
+		MaxBackups int    `yaml:"max_backups"`
+	} `yaml:"logging"`
 	Server struct {
-		Port int    `yaml:"port"`
 		Bind string `yaml:"bind"`
+		Port int    `yaml:"port"`
 	} `yaml:"server"`
-
-	Persistence struct {
-		Enabled          bool `yaml:"enabled"`
-		SnapshotInterval int  `yaml:"snapshot_interval"`
-		AppendOnly       bool `yaml:"append_only"`
-	} `yaml:"persistence"`
-
 	State struct {
 		MaxKeyAge         string `yaml:"max_key_age"`
 		MaxSize           string `yaml:"max_size"`
 		MaxAllowedEntries int    `yaml:"max_allowed_entries"`
 	} `yaml:"state"`
-
-	Logging struct {
-		Level      string `yaml:"level"`
-		File       string `yaml:"file"`
-		MaxSize    string `yaml:"max_size"`
-		MaxBackups int    `yaml:"max_backups"`
-		MaxAge     string `yaml:"max_age"`
-	} `yaml:"logging"`
+	Persistence struct {
+		SnapshotInterval int  `yaml:"snapshot_interval"`
+		Enabled          bool `yaml:"enabled"`
+		AppendOnly       bool `yaml:"append_only"`
+	} `yaml:"persistence"`
 }
 
 func LoadConfig() *Config {
 	configPath := filepath.Join("config", "config.yaml")
-
 	cfg, err := os.ReadFile(configPath)
-
 	if err != nil {
 		log.Printf("Error while reading config file %v\n", err)
 		return nil
 	}
-
 	var c Config
-
 	err = yaml.Unmarshal(cfg, &c)
-
 	if err != nil {
 		log.Printf("Error while decoding config file %v\n", err)
 		return nil
 	}
-
 	return &c
 }
