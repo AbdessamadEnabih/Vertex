4521d1bf94894efe3b6e5ec07bf146fc2b9ccc01
Abdessamad Enabih <a.enabih@gmail.com>
2024-10-31 23:12:46 +0100

    Add main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	// "github.com/AbdessamadEnabih/Vertex/cli"
+	"github.com/AbdessamadEnabih/Vertex/internal/net"
+	"github.com/AbdessamadEnabih/Vertex/internal/persistance"
+)
+
+func main() {
+
+	GlobalState, err := persistance.Load()
+
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	// cli.Execute(GlobalState)
+
+	server := net.NewServer(GlobalState)
+	if err := server.Start(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+
+	defer persistance.Save(GlobalState)
+}
