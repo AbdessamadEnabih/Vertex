c3e44efadbd0a9e497b1c8608dc588449c94c8b2
Abdessamad Enabih <a.enabih@gmail.com>
2024-11-02 18:18:49 +0100

    Create getConfigPath() function to handle both production and development environments

--- config/config.go
+++ config/config.go
@@ -35,8 +35,43 @@ type Config struct {
 	} `yaml:"persistence"`
 }
 
+func getConfigPath() string {
+	// Get environment and config path variables
+	vertexEnv := os.Getenv("VERTEX_ENV")
+	configPath := os.Getenv("VERTEX_CONFIG_PATH")
+
+	// Use the config path if explicitly set
+	if configPath != "" {
+		return configPath
+	}
+
+	// Default environment to "development" if not set
+	if vertexEnv == "" {
+		vertexEnv = "development"
+	}
+
+	// Determine config path based on environment
+	switch vertexEnv {
+	case "production":
+		configPath = "/app/config/config.yaml"
+	case "development":
+		// Use the current working directory to form the path
+		if cwd, err := os.Getwd(); err == nil {
+			configPath = filepath.Join(cwd, "config", "config.yaml")
+		} else {
+			// Fallback path if unable to get working directory
+			configPath = "config/config.yaml"
+		}
+	default:
+		// Default fallback if env is unknown
+		configPath = "config/config.yaml"
+	}
+
+	return configPath
+}
+
 func LoadConfig() *Config {
-	configPath := filepath.Join("config", "config.yaml")
+	configPath := getConfigPath()
 	cfg, err := os.ReadFile(configPath)
 	if err != nil {
 		log.Printf("Error while reading config file %v\n", err)
@@ -52,19 +87,19 @@ func LoadConfig() *Config {
 }
 
 func GetConfigByField(section string) (interface{}, error) {
-    var config Config = *LoadConfig()
+	var config Config = *LoadConfig()
 
-    v := reflect.ValueOf(config)
-    if v.Kind() != reflect.Struct {
-        return nil, fmt.Errorf("config is not a struct")
-    }
-    f := v.FieldByName(section)
-    if !f.IsValid() {
-        return nil, fmt.Errorf("field %s not found", section)
-    }
-    if f.Kind() != reflect.Struct {
-        return nil, fmt.Errorf("field %s is not a struct", section)
-    }
-    
-    return f.Interface(), nil
+	v := reflect.ValueOf(config)
+	if v.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("config is not a struct")
+	}
+	f := v.FieldByName(section)
+	if !f.IsValid() {
+		return nil, fmt.Errorf("field %s not found", section)
+	}
+	if f.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("field %s is not a struct", section)
+	}
+
+	return f.Interface(), nil
 }
