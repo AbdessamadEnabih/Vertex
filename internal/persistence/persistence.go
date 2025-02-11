c70bc373a507c6833515b1c77afe832e4d8494e0
Abdessamad Enabih <a.enabih@gmail.com>
2025-02-11 22:09:35 +0100

    refactor: enhance error logging in persistence functions for better traceability

--- internal/persistence/persistence.go
+++ internal/persistence/persistence.go
@@ -1,132 +1,130 @@
 package persistence
 
 import (
-	"bytes"
-	"compress/gzip"
-	"encoding/json"
-	"io"
-	"os"
-	"path/filepath"
-	"reflect"
+    "bytes"
+    "compress/gzip"
+    "encoding/json"
+    "io"
+    "os"
+    "path/filepath"
+    "reflect"
 
-	"github.com/AbdessamadEnabih/Vertex/pkg/config"
-	"github.com/AbdessamadEnabih/Vertex/pkg/datastore"
-	logger "github.com/AbdessamadEnabih/Vertex/pkg/logger"
+    "github.com/AbdessamadEnabih/Vertex/pkg/config"
+    "github.com/AbdessamadEnabih/Vertex/pkg/datastore"
+    logger "github.com/AbdessamadEnabih/Vertex/pkg/logger"
 )
 
 func get_datastore_path() string {
-	persistence_config, err := config.GetConfigByField("Persistence")
-	if err != nil {
-		logError("Error getting persistence config", "", err)
-	}
+    persistence_config, err := config.GetConfigByField("Persistence")
+    if err != nil {
+        logError("persistence.get_datastore_path: Error getting persistence config", "", err)
+    }
 
-	dir, _ := os.Getwd()
+    dir, _ := os.Getwd()
 
-	return filepath.Join(filepath.Join(dir, reflect.ValueOf(persistence_config).FieldByName("Path").String()), "datastore.data")
+    return filepath.Join(filepath.Join(dir, reflect.ValueOf(persistence_config).FieldByName("Path").String()), "datastore.data")
 }
 
 func Save(datastore *datastore.DataStore) error {
-	datastorepath := get_datastore_path()
+    datastorepath := get_datastore_path()
 
-	if err := WriteInDataStoreFile(datastore, datastorepath); err != nil {
-		logError("Error saving datastore", datastorepath, err)
-		return err
-	}
-	return nil
+    if err := WriteInDataStoreFile(datastore, datastorepath); err != nil {
+        logError("persistence.Save: Error saving datastore", datastorepath, err)
+        return err
+    }
+    return nil
 }
 
 func WriteInDataStoreFile(datastore *datastore.DataStore, filepath string) error {
-	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
-	if err != nil {
-		logError("Error opening file for writing", filepath, err)
-		return err
-	}
-	defer file.Close()
+    file, err := os.OpenFile(filepath, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+    if err != nil {
+        logError("persistence.WriteInDataStoreFile: Error opening file for writing", filepath, err)
+        return err
+    }
+    defer file.Close()
 
-	jsonData, err := json.Marshal(datastore)
-	if err != nil {
-		logError("Error marshaling datastore to JSON", filepath, err)
-		return err
-	}
+    jsonData, err := json.Marshal(datastore)
+    if err != nil {
+        logError("persistence.WriteInDataStoreFile: Error marshaling datastore to JSON", filepath, err)
+        return err
+    }
 
-	data := []byte(jsonData)
-	var compressedBuffer bytes.Buffer
-	gzipWriter := gzip.NewWriter(&compressedBuffer)
-	_, err = gzipWriter.Write(jsonData)
-	if err != nil {
-		logError("Error compressing data", filepath, err)
-		return err
-	}
-	gzipWriter.Close()
-	data = compressedBuffer.Bytes()
+    var compressedBuffer bytes.Buffer
+    gzipWriter := gzip.NewWriter(&compressedBuffer)
+    _, err = gzipWriter.Write(jsonData)
+    if err != nil {
+        logError("persistence.WriteInDataStoreFile: Error compressing data", filepath, err)
+        return err
+    }
+    gzipWriter.Close()
+    data := compressedBuffer.Bytes()
 
-	_, err = file.Write(data)
-	if err != nil {
-		logError("Error writing compressed data to file", filepath, err)
-		return err
-	}
+    _, err = file.Write(data)
+    if err != nil {
+        logError("persistence.WriteInDataStoreFile: Error writing compressed data to file", filepath, err)
+        return err
+    }
 
-	return nil
+    return nil
 }
 
 func ReadDataStoreFromFile(filepath string) (*datastore.DataStore, error) {
-	compressedData, err := os.ReadFile(filepath)
-	if err != nil {
-		logError("Error reading file", filepath, err)
-		return nil, err
-	}
+    compressedData, err := os.ReadFile(filepath)
+    if err != nil {
+        logError("persistence.ReadDataStoreFromFile: Error reading file", filepath, err)
+        return nil, err
+    }
 
-	compressedBuffer := bytes.NewBuffer(compressedData)
-	gzipReader, err := gzip.NewReader(compressedBuffer)
-	if err != nil {
-		logError("Error creating gzip reader", filepath, err)
-		return nil, err
-	}
-	defer gzipReader.Close()
+    compressedBuffer := bytes.NewBuffer(compressedData)
+    gzipReader, err := gzip.NewReader(compressedBuffer)
+    if err != nil {
+        logError("persistence.ReadDataStoreFromFile: Error creating gzip reader", filepath, err)
+        return nil, err
+    }
+    defer gzipReader.Close()
 
-	// Decompress the gzip data into a byte slice
-	var decompressedBuffer bytes.Buffer
-	_, err = io.Copy(&decompressedBuffer, gzipReader)
-	if err != nil {
-		logError("Error decompressing data", filepath, err)
-		return nil, err
-	}
+    var decompressedBuffer bytes.Buffer
+    _, err = io.Copy(&decompressedBuffer, gzipReader)
+    if err != nil {
+        logError("persistence.ReadDataStoreFromFile: Error decompressing data", filepath, err)
+        return nil, err
+    }
 
-	var savedDataStore datastore.DataStore
-	err = json.Unmarshal(decompressedBuffer.Bytes(), &savedDataStore)
-	if err != nil {
-		logError("Error unmarshaling JSON data", filepath, err)
-		return nil, err
-	}
+    var savedDataStore datastore.DataStore
+    err = json.Unmarshal(decompressedBuffer.Bytes(), &savedDataStore)
+    if err != nil {
+        logError("persistence.ReadDataStoreFromFile: Error unmarshaling JSON data", filepath, err)
+        return nil, err
+    }
 
-	return &savedDataStore, nil
+    return &savedDataStore, nil
 }
 
 func Load() (*datastore.DataStore, error) {
-	datastorepath := get_datastore_path()
+    datastorepath := get_datastore_path()
 
-	_, err := os.Stat(datastorepath)
-	if os.IsNotExist(err) {
-		logError("DataStore not found", datastorepath, err)
-		return datastore.NewDataStore(), nil
-	}
-	if err == nil {
-		savedDataStore, err := ReadDataStoreFromFile(datastorepath)
-		if err != nil {
-			logError("Error reading datastore file", datastorepath, err)
-			return datastore.NewDataStore(), err
-		}
-		return savedDataStore, nil
-	} else {
-		logError("Error checking file existence", datastorepath, err)
-		return datastore.NewDataStore(), err
-	}
+    _, err := os.Stat(datastorepath)
+    if os.IsNotExist(err) {
+        logError("persistence.Load: DataStore not found", datastorepath, err)
+        return datastore.NewDataStore(), nil
+    }
+    if err == nil {
+        savedDataStore, err := ReadDataStoreFromFile(datastorepath)
+        if err != nil {
+            logError("persistence.Load: Error reading datastore file", datastorepath, err)
+            return datastore.NewDataStore(), err
+        }
+        return savedDataStore, nil
+    } else {
+        logError("persistence.Load: Error checking file existence", datastorepath, err)
+        return datastore.NewDataStore(), err
+    }
 }
 
 func logError(message, filepath string, err error) {
-	if filepath != "" {
-		logger.Log(message+" at path "+filepath+": "+err.Error(), "ERROR")
-	} else {
-		logger.Log(message+": "+err.Error(), "ERROR")
-	}
+    if filepath != "" {
+        logger.Log(message+" at path "+filepath+": "+err.Error(), "ERROR")
+    } else {
+        logger.Log(message+": "+err.Error(), "ERROR")
+    }
 }
--- pkg/persistence/persistence.go
+++ pkg/persistence/persistence.go
@@ -1,32 +1,32 @@
 package persistence
 
 import (
-	"errors"
+    "errors"
 
-	"github.com/AbdessamadEnabih/Vertex/internal/persistence"
-	"github.com/AbdessamadEnabih/Vertex/pkg/datastore"
-	logger "github.com/AbdessamadEnabih/Vertex/pkg/logger"
+    "github.com/AbdessamadEnabih/Vertex/internal/persistence"
+    "github.com/AbdessamadEnabih/Vertex/pkg/datastore"
+    logger "github.com/AbdessamadEnabih/Vertex/pkg/logger"
 )
 
 func Save(datastore *datastore.DataStore) error {
-	if err := persistence.Save(datastore); err != nil {
-		logger.Log("Error saving datastore: "+err.Error(), "Error")
-		return err
-	}
-	return nil
+    if err := persistence.Save(datastore); err != nil {
+        logger.Log("persistence.Save: Error saving datastore: "+err.Error(), "Error")
+        return err
+    }
+    return nil
 }
 
 func Load() (*datastore.DataStore, error) {
-	DataStore, err := persistence.Load()
-	if err != nil {
-		logger.Log("Error loading datastore: "+err.Error(), "Error")
-		return nil, err
-	}
+    DataStore, err := persistence.Load()
+    if err != nil {
+        logger.Log("persistence.Load: Error loading datastore: "+err.Error(), "Error")
+        return nil, err
+    }
 
-	if DataStore == nil {
-		logger.Log("Error: DataStore is nil", "Error")
-		return nil, errors.New("datastore is nil")
-	}
+    if DataStore == nil {
+        logger.Log("persistence.Load: Error: DataStore is nil", "Error")
+        return nil, errors.New("datastore is nil")
+    }
 
-	return DataStore, nil
+    return DataStore, nil
 }
